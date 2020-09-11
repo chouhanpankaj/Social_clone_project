17a11de7b6c28f549aebb6deaf87f1cd47c58c42
Pankaj Chouhan <chouhanpankaj711@gmail.com>
2020-09-11 20:30:07 +0530

    signup and login  functionality added

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,6 +7,7 @@ class UserCreateForm(UserCreationForm):
 
     class Meta:
         fields = ('username', 'email', 'password1', 'password2')
+        # fields = '__all__'
         model = get_user_model()
 
     def __init__(self, *args, **kwargs):
--- accounts/views.py
+++ accounts/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 from . import forms
-
+from django.contrib.auth.views import LoginView
 
 class SignUp(CreateView):
-    form_class = forms.UserCreationForm
+    form_class = forms.UserCreateForm
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
+
+
+# class LogIn(LoginView):
+#     template_name = 'accounts/login.html'
